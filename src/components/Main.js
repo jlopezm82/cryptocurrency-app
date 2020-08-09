ab0c809a7d6071a3d29d632c78db80223edb2142
Juan David López Mejía <Jlopezm@intergrupo.com>
2020-08-09 14:30:08 -0500

    Create components folder and Main component

---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Main() {
+  return (
+    <div className="container">
+      <div className="row">
+        <h1>Hello cryptocurrency app</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Main;
